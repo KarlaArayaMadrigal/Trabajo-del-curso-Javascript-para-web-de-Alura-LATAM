517f3622a7b16b286685bcc7ca6e9668b3419c16
Daniela AM <92821034+KarlaArayaMadrigal@users.noreply.github.com>
2024-04-22 22:51:17 -0600

    Add files via upload

---
+++ AluraMidi/main.js
@@ -0,0 +1,36 @@
+function playSonido(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+let contador = 0;
+
+for(let contador = 0; contador < 9; contador ++){
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    console.log(contador)
+
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio);
+
+    tecla.onclick = function () {
+        playSonido(idAudio);
+    };
+    tecla.onkeydown = function (evento) {
+        
+        if (evento.code == 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('activa');
+        }console.log(evento.code == 'Space' || evento.code === 'Enter')
+    }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('activa');
+}
+
+
+}
+
+
+
+
+
